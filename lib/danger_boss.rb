dd7919f7b33db0cb623e6167f4d6acffe0fd9f1f
Dennis Walters <pooster@gmail.com>
2019-01-29 15:01:46 -0500

    Alerts be gotted

--- lib/danger_boss.rb
+++ lib/danger_boss.rb
@@ -1,5 +1,11 @@
-require "danger_boss/version"
+require 'danger_boss/version'
+require 'danger_boss/client'
+require 'danger_boss/driver/http'
 
 module DangerBoss
-  # Your code goes here...
+  def self.client(base_url:,username: nil, password: nil)
+    Client.new(
+      driver: Driver::Http.new(base_url: base_url, username: username, password: password)
+    )
+  end
 end
---
+++ lib/danger_boss/driver.rb
@@ -0,0 +1,4 @@
+module DangerBoss
+  module Driver
+  end
+end
